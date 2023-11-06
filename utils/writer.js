db79150a6dd06a88f48d425a2668a61b79675fdf
Nikolas Sachos <nikolas@admintuts.net>
2023-11-06 16:33:01 +0200

    Added error handling to responces

--- utils/writer.js
+++ utils/writer.js
@@ -1,43 +1,44 @@
-var ResponsePayload = function(code, payload) {
+var ResponsePayload = function (code, payload) {
   this.code = code;
   this.payload = payload;
-}
+};
 
-exports.respondWithCode = function(code, payload) {
+exports.respondWithCode = function (code, payload) {
   return new ResponsePayload(code, payload);
-}
+};
 
-var writeJson = exports.writeJson = function(response, arg1, arg2) {
-  var code;
-  var payload;
+var writeJson = exports.writeJson = function (response, arg1, arg2) {
+  var code = 200; // Default status code.
+  var payload = {};
 
-  if(arg1 && arg1 instanceof ResponsePayload) {
-    writeJson(response, arg1.payload, arg1.code);
-    return;
-  }
-
-  if(arg2 && Number.isInteger(arg2)) {
-    code = arg2;
-  }
-  else {
-    if(arg1 && Number.isInteger(arg1)) {
+  if (arg1) {
+    if (arg1 instanceof ResponsePayload) {
+      code = arg1.code;
+      payload = arg1.payload;
+    } else if (Number.isInteger(arg1)) {
       code = arg1;
+    } else if (typeof arg1 === 'object') {
+      payload = arg1;
     }
   }
-  if(code && arg1) {
-    payload = arg1;
-  }
-  else if(arg1) {
-    payload = arg1;
+
+  if (arg2 && Number.isInteger(arg2)) {
+    code = arg2;
   }
 
-  if(!code) {
-    // if no response code given, we default to 200
-    code = 200;
+  // Ensuring the responce code is a valid HTTP status code.
+  if (code < 100 || code >= 600) {
+    code = 500; // Internal Server Error as a fallback.
   }
-  if(typeof payload === 'object') {
-    payload = JSON.stringify(payload, null, 2);
+
+  // Ensure the payload is indeed an object.
+  if (typeof payload !== 'object') {
+    payload = {
+      message: 'Invalid Response Payload',
+    };
   }
-  response.writeHead(code, {'Content-Type': 'application/json'});
+
+  payload = JSON.stringify(payload, null, 2);
+  response.writeHead(code, { 'Content-Type': 'application/json' });
   response.end(payload);
-}
+};
